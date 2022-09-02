853b1518ec60fb7e4620c6d0e6ccf6f05f8700ad
The-Accolade <olusolaakolade@gmail.com>
2022-09-02 16:21:43 +0100

    first commit

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+import useStyles from './styles.js';
+
+function Posts() {
+const posts = useSelector((state) => state.posts)
+const classes = useStyles();
+
+console.log(posts);
+
+  return (
+    <>
+        <div>Posts</div>
+        <Post />
+        <Post />
+    </>
+  )
+}
+
+export default Posts
---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyPaser from 'body-parser';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+//limit to restrict files larger than 30mb
+app.use(bodyPaser.json({limit: '30mb', extended: true}));
+app.use(bodyPaser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://AccoladeDB:Accolade@cluster0.8tfcsjt.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+    .catch((err) => console.log(err.message));
